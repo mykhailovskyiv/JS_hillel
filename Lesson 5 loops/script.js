67701141ff4115f8cb8f83bcc8ca0260b1235a37
mykhailovskyiv <mykhailovskyiv@github.com>
2020-02-18 13:00:19 +0300

    работает

--- Lesson 5 loops/script.js
+++ Lesson 5 loops/script.js
@@ -1,47 +1,53 @@
+//Задание №1
 
+var i = 1;
+while (i < 100) {
+    i++;
+    if (i % 4) {
+        continue;
+    } console.log(i);
+}
 
 //Задание №2 Угадайка сгенерированного числа
 
-// function again() {
-//     yourNum = +prompt('попробуй еще раз');
-// }
-// var victory = false;
-// var number = Math.ceil(Math.random() * 10);
-// var yourNum = +prompt('введите число от 1 до 10');
+function again() {
+    yourNum = +prompt('попробуй еще раз');
+}
+var victory = false;
+var number = Math.ceil(Math.random() * 10);
+var yourNum = +prompt('введите число от 1 до 10');
 
 
 
-// do {
-//     again();
-//     if (yourNum == number) {
-//         victory = true(alert('Вы угадали'))
-//     }
-//     if (Number.isNaN(yourNum)) {
-//         alert('цифры вводи епт');
-//     }
-//     if (yourNum === null) {
-//         alert('зря');
-//     }
-// } while (!victory);
+do {
+    again();
+    if (yourNum == number) {
+        victory = true(alert('Вы угадали'))
+    }
+    if (Number.isNaN(yourNum)) {
+        alert('цифры вводи епт');
+    }
+    if (yourNum === null) {
+        alert('зря');
+    }
+} while (!victory);
 
 // Задание №3 Кратные числа 
 
-// var n = + prompt('введите число')
+var n = + prompt('введите число')
 
-// for (var i = 1; i < 100; i++) {
-//     if (i % n) continue;
-//     console.log(i);
-// }
+for (var i = 1; i < 100; i++) {
+    if (i % n) continue;
+    console.log(i);
+}
 
+// Задание №4 Кратные числа
 
-// Задание №4 Кратные числа 
+var n = + prompt('введите число')
+var y = + prompt('сколько числе нужно получить')
 
-// var n = + prompt('введите число')
-// var y = + prompt('сколько числе нужно получить')
-
-// for (var i = 1; i < (y * n + 1); i++) {
-//     if (i % n) continue;
-//     console.log(i);
-// }
-
+for (var i = 1; i < (y * n + 1); i++) {
+    if (i % n) continue;
+    console.log(i);
+}
 
