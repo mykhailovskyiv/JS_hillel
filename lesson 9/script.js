39c8155905deaa2d01bce9aa4d19cbe9faae8e59
mykhailovskyiv <mykhailovskyiv@github.com>
2020-03-09 19:39:19 +0200

    homework

--- lesson 9/script.js
+++ lesson 9/script.js
@@ -1,46 +1,75 @@
-var obj = {
-    name: "MyFutureObject",
-    age: 24,
-    sex: "male",
-    getAge: function () {
-        return this.age;
-    },
-    incAge: function () {
-        return this.age++;
+var dwarf = new MilitaryResource('Dwarf');
+
+
+function MilitaryResource(type, health, distance) {
+    this.type = type;
+    this.health = Math.floor(Math.random() * 5);
+    this.maxHealth = 5;
+    this.distance = Math.floor(Math.random() * 5);
+    this.maxDistance = 5;
+}
+
+MilitaryResource.prototype.isReadyToMove = function () {
+    return this.distance > 0;
+};
+MilitaryResource.prototype.isReadyToFight = function () {
+    return this.health > 0;
+};
+MilitaryResource.prototype.restore = function () {
+    var newDistance;
+    var newHealth;
+    if (this.distance <= 0) {
+        newDistance = 5;
+    }
+    if (this.health <= 0) {
+        newHealth = 5;
+    }
+    return this.distance = newDistance, this.health = newHealth;
+};
+
+MilitaryResource.prototype.clone = function () {
+    return obj = new this.constructor(this.type, this.health, this.distance);
+}
+
+console.log(dwarf);
+console.log(dwarf.isReadyToMove());
+console.log(dwarf.isReadyToFight());
+dwarf.restore();
+console.log(dwarf.health);
+console.log(dwarf.distance);
+
+
+
+function Squad() {
+    this.squad = [];
+}
+
+var restore = [];
+
+Squad.prototype.everyIsReadyToMove = function () {
+    return this.squad.every(function (unit) {
+        return unit.isReadyToMove() === true;
+    });
+};
+
+Squad.prototype.everyIsReadyToFight = function () {
+    return this.squad.every(function (unit) {
+        return unit.isReadyToFight() === true;
+    });
+};
+
+Squad.prototype.combineResources = function (unit) {
+    if (unit.health < 5 || unit.distance < 50) {
+        restore.push(unit);
+    } else {
+        this.squad.push(unit);
     }
 };
 
-function User(name, age, exp, sex) {
-    // {} <- this.
-    this.exp = exp;
-    this.name = name;
-    this.age = age;
-    this.sex = sex === undefined ? "female" : "male";
-    this.salary = 500;
-}
 
-User.prototype.getAge = function () {
-    return this.age;
-};
+var unitOne = new MilitaryResource(dwarf);
+var unitTwo = new MilitaryResource(dwarf);
+var unitThre = new MilitaryResource(dwarf);
+var squad = new Squad();
 
-User.prototype.incAge = function () {
-    return this.age++;
-};
-
-User.prototype.incSalary = function (bonusesAmount) {
-    var bonuses = Number(bonusesAmount) || 0;
-    return (this.salary += 500 + bonuses + (this.exp / 100) * bonuses);
-};
-
-var usersArray = [];
-
-for (var i = 0; i < 5; i++) {
-    usersArray.push(new User("User " + i, 24 + i, i + 1));
-}
-
-for (var i = 0; i < usersArray.length; i++) {
-    usersArray[i].incAge();
-    usersArray[i].incSalary(200);
-}
-
-console.log(usersArray);
+console.log(restore);
