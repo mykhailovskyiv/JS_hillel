64b5f8d91a678e1dce55d1993cbeb717d79fecc9
mykhailovskyiv <mykhailovskyiv@github.com>
2020-05-01 19:06:34 +0300

    lesson update

--- lesson 20/express-server.js
+++ lesson 20/express-server.js
@@ -1,27 +1,48 @@
 var express = require('express');
 var app = express();
 
-var arr = []
+function user(id, name) {
+    this.id = id || 'id';
+    this.name = name || 'User Name';
+};
 
-var generateObject = function () {
-    return {
-        value: (Math.random() * 100) >> 0,
+
+function arrUsers(arrlenght) {
+    var arr = [];
+    for (let i = 0; i < arrlenght; i++) {
+        arr.push(new user(i + 1, 'User Name' + (i + 1)));
     }
+    return arr;
 }
 
-app.get('/', function (req, res) {
-    res.send('App works!')
+var arrOfUsers = arrUsers(10).map(function (id) {
+    return id
 })
 
-app.post ('favorites', function (req, res) {
-    
-})
+
+app.use(express.json());
+
 
 app.get('/', function (req, res) {
-    arr.push(generateObject());
-    res.sjson(arr);
+    res.json(arrOfUsers);
+    res.send(arrOfUsers);
+});
+
+app.post('/', function (req, res) {
+    arrOfUsers.push(req.body);
+    res.json(req.body);
+});
+
+app.delete('/:id', function (req, res) {
+    const index = arrOfUsers.findIndex(function (value) {
+        return value.id === req.body.id;
+    });
+    arrOfUsers.splice(index, 1);
+    res.json(req.body);
 });
 
 app.listen(3000, function () {
-    console.log('App is on listening port 3000');
+    console.log('listening port: 3000');
 });
+
+console.log(arrOfUsers)
--- lesson 20/http-example.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('hello' + ++inc);
-});
-
-server.listen('8000');
